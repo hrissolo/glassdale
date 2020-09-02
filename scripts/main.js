0571b3181847eefe39e73fccce6f2f8136968d91
hrissolo <hrissolo@gmail.com>
2020-09-02 09:33:28 -0500

    filtered

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,5 @@ import {OfficerList} from './officers/OfficerList.js'
 import {CriminalList} from './criminals/CriminalList.js'
 import {ConvictionSelect} from './convictions/ConvictionSelect.js'
 
-OfficerList()
 CriminalList()
 ConvictionSelect()
---
+++ scripts/officers/OfficerSelect.js
@@ -0,0 +1,21 @@
+import {getOfficers, useOfficers} from './OfficerProvider.js'
+
+const eventHub = document.querySelector(".container")
+
+
+eventHub.addEventListener("change", changeEvent => {
+    if (changeEvent.target.id === "officerSelect") {
+        // Get the name of the selected officer
+        const selectedOfficer = changeEvent.target.value
+
+        // Define a custom event
+        const customEvent = new CustomEvent("officerSelected", {
+            detail: {
+                officer: selectedOfficer
+            }
+        })
+
+        // Dispatch event to event hub
+        eventHub.dispatchEvent(customEvent)
+    }
+})
